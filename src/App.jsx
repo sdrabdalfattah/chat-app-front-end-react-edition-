7e852ceabeb30d5d696ad732e502b69541230816
sdrabdalfattah <sdrabdalfattah@gmail.com>
2025-07-28 12:14:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LargeComponent from './Large_component.jsx'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { BrowserRouter } from 'react-router-dom';
+import { useState, useEffect  } from "react";
 
 function App() {
 const darkTheme = createTheme({
@@ -11,6 +12,28 @@ const darkTheme = createTheme({
     mode: 'dark',
   },
 });
+
+const [inputBottom, setInputBottom] = useState(0);
+
+useEffect(() => {
+  const handleViewportResize = () => {
+    const vp = window.visualViewport;
+    if (vp) {
+      const keyboardHeight = window.innerHeight - vp.height - vp.offsetTop;
+      setInputBottom(keyboardHeight > 0 ? keyboardHeight : 0);
+    }
+  };
+
+  if (window.visualViewport) {
+    window.visualViewport.addEventListener("resize", handleViewportResize);
+  }
+
+  return () => {
+    if (window.visualViewport) {
+      window.visualViewport.removeEventListener("resize", handleViewportResize);
+    }
+  };
+}, []);
 
 
   return (
@@ -23,6 +46,7 @@ const darkTheme = createTheme({
           margin: 0,
           width: '100%',
           padding:0,
+          transform: `translateY(-${inputBottom}px)`,
           minHeight: '100vh',
           display: 'flex',
           alignItems: 'center',
