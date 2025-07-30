ea23fdaaddad30a26abc1a091f28da34157f99e4
sdrabdalfattah <sdrabdalfattah@gmail.com>
2025-07-30 14:22:24 +0200

    Update socket.jsx

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,3 +1,3 @@
 // socket.js
 import { io } from "socket.io-client";
-export const socket = io("https://chat-app-backend-z319.onrender.com");
+export const socket = io("https://chat-app-backend-1-tni2.onrender.com");
